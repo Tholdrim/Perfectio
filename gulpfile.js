eaddecc18a65d5c1d55e47bf0c55e7d015a463cb
Jan Walczyński <jan.walczynski@perfectio.pl>
2022-01-01 00:10:23 +0100

    Updated dependencies and Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,50 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-const htmlmin = require('gulp-htmlmin');
-const less = require('gulp-less');
-const rename = require('gulp-rename');
 const pump = require('pump');
-const http = require('http');
-const finalhandler = require('finalhandler');
-const serveStatic = require('serve-static');
+const browserSync = require('browser-sync').create();
+const gulp = Object.assign(require('gulp'), { cleanCss: require('gulp-clean-css'), minifyHtml: require('gulp-htmlmin'), less: require('gulp-less'), rename: require('gulp-rename') });
 
-gulp.task('assets', callback => {
+function assets(callback) {
     pump([
         gulp.src('source/assets/*.*'),
-        gulp.dest('result/assets'),
+        gulp.dest('result/assets')
     ], callback);
-});
+}
 
-gulp.task('other', callback => {
-    pump([
-        gulp.src('source/other/*.*'),
-        gulp.dest('result'),
-    ], callback);
-});
-
-gulp.task('less', callback => {
+function less(callback) {
     pump([
         gulp.src('source/styles/styles.less'),
-        less({ math: 'always' }),
+        gulp.less({ math: 'always' }),
         gulp.dest('result'),
-        rename({ suffix: '.min' }),
-        cleanCSS({ level: 2 }),
+        gulp.rename({ suffix: '.min' }),
+        gulp.cleanCss({ level: 2 }),
         gulp.dest('result')
     ], callback);
-});
+}
 
-gulp.task('minify', callback => {
+function minify(callback) {
     pump([
         gulp.src('source/views/*.html'),
-        htmlmin({ collapseWhitespace: true, removeComments: true }),
+        gulp.minifyHtml({ collapseWhitespace: true, removeComments: true }),
         gulp.dest('result')
     ], callback);
-});
+}
 
-gulp.task('watch', () => {
-    gulp.watch('source/assets/*.*', gulp.series('assets'));
-    gulp.watch('source/other/*.*', gulp.series('other'));
-    gulp.watch('source/styles/*.less', gulp.series('less'));
-    gulp.watch('source/views/*.html', gulp.series('minify'));
-});
+function other(callback) {
+    pump([
+        gulp.src('source/other/*.*'),
+        gulp.dest('result')
+    ], callback);
+}
 
-gulp.task('server', () => {
-    const serve = serveStatic('result');
- 
-    http.createServer((request, response) => {
-        serve(request, response, finalhandler(request, response));
-    }).listen(8080);
-});
+function server() {
+    browserSync.init({ port: 8080, server: 'result', ui: false, watch: true });
+}
 
-gulp.task('build', gulp.series(['assets', 'other', 'minify', 'less']));
+function watch() {
+    gulp.watch('source/assets/*.*', gulp.series(assets));
+    gulp.watch('source/other/*.*', gulp.series(other));
+    gulp.watch('source/styles/*.less', gulp.series(less));
+    gulp.watch('source/views/*.html', gulp.series(minify));
+}
 
-gulp.task('default', gulp.series('build', gulp.parallel('watch', 'server')));
+exports.build =  gulp.series(assets, less, minify, other);
+exports.default = gulp.series(exports.build, gulp.parallel(watch, server));
